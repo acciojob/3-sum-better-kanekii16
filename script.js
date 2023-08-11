feb10053d1bf56d0bc96c3a1918f6e0f976792df
kanekii16 <96989943+kanekii16@users.noreply.github.com>
2023-08-11 15:46:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 function threeSum(arr, target) {
 // write your code here
+
+	   arr.sort();
+
+     
+
+       let closestSum = arr[0]+arr[1]+arr[2];
+
+       for(let i = 0 ; i < arr.length - 2 ; i++){
+            let j = i+1;
+            let k = arr.length-1;
+        while(j < k){
+            let sum = arr[i] + arr[j] + arr[k];
+
+        if(sum == target){
+            return target;
+        }
+        if(Math.abs(target-sum) < Math.abs(target-closestSum)){
+            closestSum = sum;
+        }
+
+        if(sum < target){
+            j++;
+        }else{
+            k--;
+        }
+
+        }
+       }
+
+       return closestSum;
   
 }
 
